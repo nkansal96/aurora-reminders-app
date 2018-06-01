ea7906bc126f49a0db7bcafb6b9eed1b8290c63f
ritamsarmah <sarmah.ritam@gmail.com>
2018-05-31 18:03:57 -0700

    UI improvements and fixed speech response

--- speech.py
+++ speech.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.config import Config
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
@@ -22,11 +23,15 @@ theme_color = [0.31, 0.89, 0.76, 1]
 white_color = [1, 1, 1, 1]
 black_color = [0, 0, 0, 1]
 listen_msg = "Listening..."
-confirmations = ['OK. ', 'Got it. ', 'Sure. ', 'All right. ', '']
+confirmations = ['OK. ', 'Got it. ', 'Sure. ', 'Alright. ', '']
+apologies = ["I'm sorry, I don't understand.", "I didn't understand that."]
 cancel_intents = ['cancel', 'never mind', 'forget it', 'quit', 'restart', 'start over']
 
 def get_random_confirmation():
     return confirmations[randint(0, len(confirmations) - 1)]
+
+def get_random_apology():
+    return apologies[randint(0, len(apologies) - 1)]
 
 event_mgr = EventManager()
 
@@ -68,7 +73,7 @@ class ChatApp(App):
             interpret = text.interpret()
 
             if interpret.intent != 'set_reminder':
-                update_chat("I'm sorry, I don't understand.")
+                update_chat(get_random_apology())
                 return False
             else:
                 create_event(interpret)
@@ -78,6 +83,9 @@ class ChatApp(App):
             update_chat("Creating your reminder: \"{}\".".format(interpret.entities['task'].capitalize()), confirm=True)
             event_mgr.convert_text_to_event(interpret)
 
+        def play_text_callback(text, *largs):
+            Text(text).speech().audio.play()
+
         def listen_callback(*largs):
             msg = listen_and_transcribe(silence_len=0.5)
             hide_listen_animation()
@@ -85,16 +93,18 @@ class ChatApp(App):
             if msg.text != '':
                 update_chat(msg.text, is_user=True)
                 interpret_user_response(msg)
+            else:
+                update_chat(get_random_apology())
 
         def record_user_response():
             show_listen_animation()
             Clock.schedule_once(listen_callback, 0)
 
         def show_listen_animation():
-            chat_view.text += listen_msg
+            button.text = listen_msg
 
         def hide_listen_animation():
-            chat_view.text = chat_view.text[:-len(listen_msg)]
+            button.text = "Record"
 
         def update_chat(text, is_user=False, confirm=False):
             if is_user:
@@ -103,9 +113,11 @@ class ChatApp(App):
                 response = ""
                 if confirm:
                     response += get_random_confirmation()
-                response += text + '\n'
+                response += text
 
-                chat_view.text += response
+                Clock.schedule_once(partial(play_text_callback, response), 0)
+
+                chat_view.text += response + '\n'
 
         button.bind(on_press=lambda x: record_user_response())
 
@@ -121,5 +133,9 @@ if __name__ == "__main__":
     # Set your application settings
     aurora.config.app_id    = APP_ID     # put your app ID here
     aurora.config.app_token = APP_TOKEN  # put your app token here
+
+    Config.set('graphics', 'width', '500')
+    Config.set('graphics', 'height', '600')
+    Config.write()
 
     ChatApp().run()
